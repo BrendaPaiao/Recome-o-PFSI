a9337224ac6615a09e1c0b5f18a9db0dcb132a14
BrendaPaiao <brendamelop8@gmail.com>
2025-09-22 01:45:48 -0300

    Feat: Criando a UsuarioRoute

---
+++ Routes/usuarioRoute.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const UsuarioController = require('../Controllers/usuarioController');
+
+const router =  express.Router();
+
+// Cria uma instância da UsuarioController para acessar as funções dela (listarView, cadastrarView, etc.).
+let ctrl = new UsuarioController();
+
+//** GET(Pegar): usado quando o usuário acessa uma URL (pedir dados ou mostrar uma página).**
+
+// Rota GET "/usuario/"
+// Quando o usuário acessar /usuario (Quando o usuário acessar /usuario, pode ser porque ele clicou em um link, digitou a URL ou foi redirecionado), chama a função listarView da controller.
+router.get("/", ctrl.listarView);
+// Rota GET "/usuario/cadastrar".
+// Quando o usuário acessar /usuario/cadastrar, chama a função cadastrarView.
+// (normalmente renderiza o formulário de cadastro).
+router.get("/cadastrar", ctrl.cadastrarView);
+
+module.exports = router;
